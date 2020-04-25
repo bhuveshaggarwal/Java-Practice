a46a9bf19d2ea97aa03007c41348950b170d9413
bhuveshaggarwal <bhuveshaggarwal@gmail.com>
2020-04-25 14:31:46 +0530

    Daily practice.

--- src/com/bhuvesh/Practice.java
+++ src/com/bhuvesh/Practice.java
@@ -1,4 +1,27 @@
 package com.bhuvesh;
 
 public class Practice {
+    int num ;
+    String name;
+
+    Practice(){
+        System.out.println("No-Argument Constructor");
+    }
+
+    Practice(String name, int num){
+        this.name = name;
+        this.num = num;
+        System.out.println("Parameterized Constructor");
+    }
+
+
+    //Constructor Overloading
+    Practice(String names){
+        System.out.println("Constructor with one Argument");
+    }
+
+    Practice(long id, int num){
+        System.out.println("Constructor with two arguments");
+    }
+    
 }
